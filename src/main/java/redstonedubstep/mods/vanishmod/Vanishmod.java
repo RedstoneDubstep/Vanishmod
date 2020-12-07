06e185a177598b1879c4db221da7e478f34ade2c
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2020-12-07 18:26:40 +0100

    Fixed servers with this mod displaying an incompatible mod list warning

--- src/main/java/redstonedubstep/mods/vanishmod/Vanishmod.java
+++ src/main/java/redstonedubstep/mods/vanishmod/Vanishmod.java
@@ -2,13 +2,18 @@ package redstonedubstep.mods.vanishmod;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod(Vanishmod.MODID)
 public class Vanishmod {
     public static final String MODID = "vanishmod";
 
 	public Vanishmod() {
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 		MinecraftForge.EVENT_BUS.addListener(this::registerCommands);
 	}
 
