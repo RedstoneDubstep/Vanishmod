982a88d91f83f9f46b98e54b873cfdea6bec7ae6
Tim Eßer <essertim13@googlemail.com>
2020-12-08 20:53:19 +0100

    Revert "better vanish"
    This reverts commit fe72be46

--- src/main/java/redstonedubstep/mods/vanishmod/VanishUtil.java
+++ src/main/java/redstonedubstep/mods/vanishmod/VanishUtil.java
@@ -11,84 +11,81 @@ import net.minecraft.command.CommandSource;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
-import net.minecraft.network.play.server.SDestroyEntitiesPacket;
 import net.minecraft.network.play.server.SEntityEquipmentPacket;
 import net.minecraft.network.play.server.SPlayerListItemPacket;
 import net.minecraft.network.play.server.SPlayerListItemPacket.Action;
-import net.minecraft.network.play.server.SSpawnPlayerPacket;
 import net.minecraft.util.text.IFormattableTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.server.ServerChunkProvider;
 
 public class VanishUtil {
-    private static final List<UUID> vanishedPlayers = Lists.newArrayList();
+	private static final List<UUID> vanishedPlayers = Lists.newArrayList();
 
-    public static List<ServerPlayerEntity> formatPlayerList(List<ServerPlayerEntity> rawList) {
-        List<ServerPlayerEntity> formattedList = Lists.newArrayList();
+	public static List<ServerPlayerEntity> formatPlayerList(List<ServerPlayerEntity> rawList) {
+		List<ServerPlayerEntity> formattedList = Lists.newArrayList();
 
-        for (ServerPlayerEntity player : rawList) {
-            if (!vanishedPlayers.contains(player.getUniqueID()))
-                formattedList.add(player);
-        }
+		for (ServerPlayerEntity player : rawList) {
+			if (!vanishedPlayers.contains(player.getUniqueID()))
+				formattedList.add(player);
+		}
 
-        return formattedList;
-    }
+		return formattedList;
+	}
 
-    public static void sendPacketsOnVanish(CommandContext<CommandSource> ctx, boolean vanished) throws CommandSyntaxException {
-        ServerPlayerEntity currentPlayer = ctx.getSource().asPlayer();
+	public static void sendPacketsOnVanish(CommandContext<CommandSource> ctx, boolean vanished) throws CommandSyntaxException {
+		ServerPlayerEntity currentPlayer = ctx.getSource().asPlayer();
 
-        List<ServerPlayerEntity> list = ctx.getSource().getWorld().getPlayers();
+		List<ServerPlayerEntity> list = ctx.getSource().getWorld().getPlayers();
 
-        for (ServerPlayerEntity player : list) {
-            ServerChunkProvider chunkProvider = player.getServerWorld().getChunkProvider();
+		for (ServerPlayerEntity player : list) {
+			ServerChunkProvider chunkProvider = player.getServerWorld().getChunkProvider();
 
-            if (!player.equals(currentPlayer)) { //prevent packet from being sent to the executor of the command
-                player.connection.sendPacket(new SPlayerListItemPacket(vanished ? Action.REMOVE_PLAYER : Action.ADD_PLAYER, currentPlayer));
-                if (!vanished) {
-                    player.connection.sendPacket(new SSpawnPlayerPacket(currentPlayer));
-//                    chunkProvider.chunkManager.entities.remove(currentPlayer.getEntityId());
-//                    chunkProvider.track(currentPlayer);
-                } else {
-                    player.connection.sendPacket(new SDestroyEntitiesPacket(currentPlayer.getEntityId()));
-//					hidePlayerArmor(currentPlayer, player);
-                }
-            }
-        }
-    }
+			if (!player.equals(currentPlayer)) { //prevent packet from being sent to the executor of the command
+				player.connection.sendPacket(new SPlayerListItemPacket(vanished ? Action.REMOVE_PLAYER : Action.ADD_PLAYER, currentPlayer));
+				if (!vanished) {
+					chunkProvider.chunkManager.entities.remove(currentPlayer.getEntityId());
+					chunkProvider.track(currentPlayer);
+				}
+				else {
+					hidePlayerArmor(currentPlayer, player);
+				}
+			}
+		}
+	}
 
-    private static void hidePlayerArmor(ServerPlayerEntity vanishedPlayer, ServerPlayerEntity otherPlayer) {
-        List<Pair<EquipmentSlotType, ItemStack>> list = Lists.newArrayList();
+	private static void hidePlayerArmor(ServerPlayerEntity vanishedPlayer, ServerPlayerEntity otherPlayer) {
+		List<Pair<EquipmentSlotType, ItemStack>> list = Lists.newArrayList();
 
-        for (EquipmentSlotType equipmentslottype : EquipmentSlotType.values()) {
-            list.add(Pair.of(equipmentslottype, ItemStack.EMPTY));
-        }
+		for(EquipmentSlotType equipmentslottype : EquipmentSlotType.values()) {
+			list.add(Pair.of(equipmentslottype, ItemStack.EMPTY));
+		}
 
-        otherPlayer.connection.sendPacket(new SEntityEquipmentPacket(vanishedPlayer.getEntityId(), list));
-    }
+		otherPlayer.connection.sendPacket(new SEntityEquipmentPacket(vanishedPlayer.getEntityId(), list));
+	}
 
-    public static void sendMessageToAllPlayers(List<ServerPlayerEntity> playerList, ServerPlayerEntity sender, boolean vanished) {
-        IFormattableTextComponent content = new TranslationTextComponent(vanished ? "multiplayer.player.left" : "multiplayer.player.joined", sender.getDisplayName()).mergeStyle(TextFormatting.YELLOW);
+	public static void sendMessageToAllPlayers(List<ServerPlayerEntity> playerList, ServerPlayerEntity sender, boolean vanished) {
+		IFormattableTextComponent content = new TranslationTextComponent(vanished ? "multiplayer.player.left" : "multiplayer.player.joined", sender.getDisplayName()).mergeStyle(TextFormatting.YELLOW);
 
-        for (ServerPlayerEntity receiver : playerList) {
-            receiver.sendMessage(content, sender.getUniqueID());
-        }
-    }
+		for (ServerPlayerEntity receiver : playerList) {
+			receiver.sendMessage(content, sender.getUniqueID());
+		}
+	}
 
-    public static void updateVanishedList(ServerPlayerEntity player, boolean vanished) {
-        if (vanished)
-            vanishedPlayers.add(player.getUniqueID());
-        else
-            vanishedPlayers.remove(player.getUniqueID());
+	public static void updateVanishedList(ServerPlayerEntity player, boolean vanished) {
+		if (vanished)
+			vanishedPlayers.add(player.getUniqueID());
+		else
+			vanishedPlayers.remove(player.getUniqueID());
 
-        player.setInvisible(vanished);
-    }
+		player.setInvisible(vanished);
+	}
 
-    public static boolean isVanished(ServerPlayerEntity player) {
-        return isVanished(player.getUniqueID());
-    }
+	public static boolean isVanished(ServerPlayerEntity player) {
+		return isVanished(player.getUniqueID());
+	}
 
-    public static boolean isVanished(UUID uuid) {
-        return vanishedPlayers.contains(uuid);
-    }
+	public static boolean isVanished(UUID uuid) {
+		return vanishedPlayers.contains(uuid);
+	}
 }
