a951b6aae1c1bbb6e15816c2eaff22ba562ae887
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2021-12-05 22:01:05 +0100

    Updated to 1.18

--- src/main/java/redstonedubstep/mods/vanishmod/Vanishmod.java
+++ src/main/java/redstonedubstep/mods/vanishmod/Vanishmod.java
@@ -6,14 +6,14 @@ import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(Vanishmod.MODID)
 public class Vanishmod {
 	public static final String MODID = "vanishmod"; //This is Vanishmod v1.1.2 for 1.17.1!
 
 	public Vanishmod() {
-		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 		MinecraftForge.EVENT_BUS.addListener(this::registerCommands);
 		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, VanishConfig.SERVER_SPEC);
 	}
