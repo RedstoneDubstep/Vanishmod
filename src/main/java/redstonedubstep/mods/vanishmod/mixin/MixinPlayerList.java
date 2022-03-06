11f42a328eec04c9b4119d9fdf3b5e626c465c59
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2022-03-06 17:45:43 +0100

    Moved join/leave/death/adv message suppression to fix mod compat

--- src/main/java/redstonedubstep/mods/vanishmod/mixin/MixinPlayerList.java
+++ src/main/java/redstonedubstep/mods/vanishmod/mixin/MixinPlayerList.java
@@ -5,20 +5,28 @@ import java.util.List;
 import java.util.UUID;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.network.Connection;
-import net.minecraft.network.protocol.game.ClientboundPlayerInfoPacket;
-import net.minecraft.server.players.PlayerList;
 import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.network.protocol.game.ClientboundPlayerInfoPacket;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.PlayerList;
+import net.minecraftforge.server.ServerLifecycleHooks;
 import redstonedubstep.mods.vanishmod.VanishUtil;
 
 @Mixin(PlayerList.class)
 public abstract class MixinPlayerList {
+	@Unique
+	private ServerPlayer joiningPlayer;
+
 	//Remove vanished players from tab list information packet that is sent to all players on connect
 	@Redirect(method = "placeNewPlayer", at = @At(value = "NEW", target = "net/minecraft/network/protocol/game/ClientboundPlayerInfoPacket", ordinal = 0))
 	public ClientboundPlayerInfoPacket constructPacketToAll(ClientboundPlayerInfoPacket.Action actionIn, ServerPlayer[] playersIn) {
@@ -43,12 +51,32 @@ public abstract class MixinPlayerList {
 		return new ClientboundPlayerInfoPacket(actionIn, VanishUtil.formatPlayerList(Arrays.asList(playersIn)));
 	}
 
-	//Block join message when player is vanished and notifies vanished player that it is still vanished
-	@Redirect(method = "placeNewPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;broadcastMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/ChatType;Ljava/util/UUID;)V"))
-	public void redirectBroadcastMessage(PlayerList playerList, Component content, ChatType chatType, UUID uuid, Connection netManager, ServerPlayer player) {
-		if (!VanishUtil.isVanished(player))
-			playerList.broadcastMessage(content, chatType, uuid);
-		else
+	//Prevent join, leave, death and advancement messages of vanished players from being broadcast
+	@Inject(method = "broadcastMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/ChatType;Ljava/util/UUID;)V", at = @At(value = "HEAD"), cancellable = true)
+	public void redirectBroadcastMessage(Component text, ChatType chatType, UUID uuid, CallbackInfo callbackInfo) {
+		if (text instanceof TranslatableComponent component) {
+			if (component.getKey().startsWith("multiplayer.player.joined") && VanishUtil.isVanished(joiningPlayer)) {
+				joiningPlayer = null;
+				callbackInfo.cancel();
+			}
+			else if (component.getKey().startsWith("multiplayer.player.left") || component.getKey().startsWith("death.") || component.getKey().startsWith("chat.type.advancement")) {
+				if (component.getArgs()[0] instanceof Component playerName) {
+					for (ServerPlayer player : ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayers()) {
+						if (player.getDisplayName().getString().equals(playerName.getString()) && VanishUtil.isVanished(player))
+							callbackInfo.cancel();
+					}
+				}
+			}
+		}
+	}
+
+	//Notify vanished players that they are still vanished when they join the server
+	@Inject(method = "placeNewPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;broadcastMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/ChatType;Ljava/util/UUID;)V"))
+	public void onSendJoinMessage(Connection networkManager, ServerPlayer player, CallbackInfo ci) {
+		if (VanishUtil.isVanished(player)) {
 			player.sendMessage(new TextComponent("Note: You are still vanished"), player.getUUID());
+		}
+
+		joiningPlayer = player;
 	}
 }
--- src/main/java/redstonedubstep/mods/vanishmod/mixin/MixinServerPlayer.java
+++ src/main/java/redstonedubstep/mods/vanishmod/mixin/MixinServerPlayer.java
@@ -3,19 +3,14 @@ package redstonedubstep.mods.vanishmod.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.mojang.authlib.GameProfile;
 
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.GameRules;
-import net.minecraft.world.level.GameRules.BooleanValue;
-import net.minecraft.world.level.GameRules.Key;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.server.level.ServerLevel;
 import redstonedubstep.mods.vanishmod.VanishConfig;
 import redstonedubstep.mods.vanishmod.VanishUtil;
 
@@ -25,15 +20,6 @@ public abstract class MixinServerPlayer extends Player {
 	//player entity needs a constructor, so here we go
 	public MixinServerPlayer(Level world, BlockPos pos, float f, GameProfile gameProfile) {
 		super(world, pos, f, gameProfile);
-	}
-
-	//suppress death messages when player is vanished by modification of the method that usually gets the value of the Gamerule showDeathMessages
-	@Redirect(method = "die", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/GameRules;getBoolean(Lnet/minecraft/world/level/GameRules$Key;)Z", ordinal = 0))
-	public boolean redirectGetBoolean(GameRules gameRules, Key<BooleanValue> key) {
-		if (VanishUtil.isVanished(getUUID(), (ServerLevel)getCommandSenderWorld()))
-			return false;
-
-		return gameRules.getBoolean(key);
 	}
 
 	//hacky mixin that should improve mod compat: mods should always respect spectator mode when targeting players, and this mixin lets isSpectator also check if the player is vanished (and thus should also not be targeted); but don't interfere with Vanilla's isSpectator() calls, else weird glitches can happen
