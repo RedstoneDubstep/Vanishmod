42d80d49dc26fff64545dfbf459fa9ba75937dd6
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2022-03-28 15:14:23 +0200

    Changed config option comments

--- src/main/java/redstonedubstep/mods/vanishmod/VanishConfig.java
+++ src/main/java/redstonedubstep/mods/vanishmod/VanishConfig.java
@@ -41,13 +41,13 @@ public class VanishConfig {
 					.comment("Should vanished players be hidden from player lists such as the /list command and the list in the Multiplayer screen?")
 					.define("hidePlayersFromPlayerLists", true);
 			hidePlayersFromCommandSelectors = builder
-					.comment("Should vanished players not be targetable by normal players via command selectors (so non-admins cannot target vanished players with e.g. /msg)?")
+					.comment("Should vanished players not be targetable by other players via command selectors (so players that cannot see vanished players cannot target them with e.g. /msg or /give)?")
 					.define("hidePlayersFromCommandSelectors", true);
 			hidePlayerNameInChat = builder
 					.comment("When vanished players send a chat message, should the name of the player that sent the message be replaced with \"vanished\" (in gray color)?")
 					.define("hidePlayerNameInChat", false);
 			sendJoinLeaveMessages = builder
-					.comment("Should other players see a join/leave message in their chat when a player (un-)vanishes?")
+					.comment("Should players see a fake join/leave message in their chat when another player (un-)vanishes?")
 					.define("sendJoinLeaveMessages", true);
 			fixModCompat = builder
 					.comment("Should there be a (potential) fix for other mods uncovering the presence of vanished players? This may severely increase CPU usage and is thus not recommended")
@@ -57,23 +57,23 @@ public class VanishConfig {
 					.define("vanishedPlayersSeeEachOther", false);
 
 			requiredPermissionLevel = builder
-					.comment("What op permission level should be the requirement for being able to execute /vanish?")
+					.comment("What op permission level should be the requirement for being able to execute /vanish? (A value of 2 or lower allows command blocks to execute /vanish)")
 					.defineInRange("requiredPermissionLevel", 2, 0, 4);
 			seeVanishedPermissionLevel = builder
-					.comment("What op permission level should be the requirement for being able to see vanished players? A value of -1 disables this feature.")
+					.comment("What op permission level should be the requirement for being able to see vanished players, no matter if the player with that permission level is vanished or not? A value of -1 disables this feature.")
 					.defineInRange("seeVanishedPermissionLevel", -1, -1, 4);
 
 			onVanishMessage = builder
-					.comment("What message should the vanishing player receive if they vanish? (%s will get replaced with the name of the vanishing player)")
+					.comment("What message should a player receive when they vanish? (%s will get replaced with the name of the vanishing player)")
 					.define("onVanishMessage", "%s vanished");
 			onUnvanishMessage = builder
-					.comment("What message should the now visible player receive if they unvanish? (%s will get replaced with the name of the unvanishing player)")
+					.comment("What message should a player receive when they unvanish? (%s will get replaced with the name of the unvanishing player)")
 					.define("onUnvanishMessage", "%s unvanished");
 			onVanishQuery = builder
-					.comment("What message should the player receive if they query their vanished status while they are vanished? (%s will get replaced with the name of the querying player)")
+					.comment("What message should a player receive if they query the vanished status of a vanished player? (%s will get replaced with the name of the player that the status is queried of)")
 					.define("onVanishQuery", "%s is currently vanished.");
 			onUnvanishQuery = builder
-					.comment("What message should the player receive if they query their vanished status while they are not vanished? (%s will get replaced with the name of the querying player)")
+					.comment("What message should a player receive if they query the vanished status of a visible player? (%s will get replaced with the name of the player that the status is queried of)")
 					.define("onUnvanishQuery", "%s is currently not vanished.");
 		}
 	}
