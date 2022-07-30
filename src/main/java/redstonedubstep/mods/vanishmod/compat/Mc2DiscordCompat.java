3e6074b0dd898d01fe4cc142da8644c540d7503e
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2022-07-30 22:43:05 +0200

    Commented out mc2discord compat for now

--- src/main/java/redstonedubstep/mods/vanishmod/compat/Mc2DiscordCompat.java
+++ src/main/java/redstonedubstep/mods/vanishmod/compat/Mc2DiscordCompat.java
@@ -1,21 +1,10 @@
 package redstonedubstep.mods.vanishmod.compat;
 
-import java.util.Collections;
-
-import com.mojang.authlib.GameProfile;
-
-import ml.denisd3d.mc2discord.core.Mc2Discord;
-import ml.denisd3d.mc2discord.core.config.M2DConfig;
-import ml.denisd3d.mc2discord.core.entities.Entity;
-import ml.denisd3d.mc2discord.core.entities.Player;
-import ml.denisd3d.mc2discord.forge.MinecraftImpl;
-import ml.denisd3d.mc2discord.forge.storage.HiddenPlayerEntry;
-import ml.denisd3d.mc2discord.forge.storage.HiddenPlayerList;
 import net.minecraft.server.level.ServerPlayer;
 
 public class Mc2DiscordCompat {
 	public static void hidePlayer(ServerPlayer player, boolean hide) {
-		HiddenPlayerList list = ((MinecraftImpl)Mc2Discord.INSTANCE.iMinecraft).hiddenPlayerList;
+		/*HiddenPlayerList list = ((MinecraftImpl)Mc2Discord.INSTANCE.iMinecraft).hiddenPlayerList;
 		GameProfile profile = player.getGameProfile();
 
 		if (hide) {
@@ -24,14 +13,14 @@ public class Mc2DiscordCompat {
 		}
 		else if (list.isHidden(profile)) {
 			list.remove(new HiddenPlayerEntry(profile));
-		}
+		}*/
 	}
 
 	public static void sendFakeJoinLeaveMessage(ServerPlayer player, boolean left) {
-		Player mc2dcplayer = new ml.denisd3d.mc2discord.core.entities.Player(player.getGameProfile().getName(), player.getDisplayName().getString(), player.getGameProfile().getId());
+		/*Player mc2dcplayer = new ml.denisd3d.mc2discord.core.entities.Player(player.getGameProfile().getName(), player.getDisplayName().getString(), player.getGameProfile().getId());
 		M2DConfig config = Mc2Discord.INSTANCE.config;
 
 		if (Mc2Discord.INSTANCE.isDiscordRunning())
-			Mc2Discord.INSTANCE.messageManager.sendInfoMessage(Entity.replace(left ? config.messages.leave : config.messages.join, Collections.singletonList(mc2dcplayer)));
+			Mc2Discord.INSTANCE.messageManager.sendInfoMessage(Entity.replace(left ? config.messages.leave : config.messages.join, Collections.singletonList(mc2dcplayer)));*/
 	}
 }
