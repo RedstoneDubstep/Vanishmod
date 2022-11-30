aa03f24d7c56a70fbf542b541d6fb5f86f3a04e2
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2022-11-30 17:59:34 +0100

    Fixed some entities like passive mobs or pufferfish detecting vanished players

--- src/main/java/redstonedubstep/mods/vanishmod/VanishUtil.java
+++ src/main/java/redstonedubstep/mods/vanishmod/VanishUtil.java
@@ -145,12 +145,12 @@ public class VanishUtil {
 		return player.level.registryAccess().registryOrThrow(Registry.CHAT_TYPE_REGISTRY).getResourceKey(chatType.chatType()).orElse(ChatType.CHAT);
 	}
 
-	public static boolean isVanished(Player player) {
+	public static boolean isVanished(Entity player) {
 		return isVanished(player, null);
 	}
 
-	public static boolean isVanished(Player player, Entity forPlayer) {
-		if (player != null && !player.level.isClientSide) {
+	public static boolean isVanished(Entity player, Entity forPlayer) {
+		if (player instanceof Player && !player.level.isClientSide) {
 			boolean isVanished = player.getPersistentData().getCompound(Player.PERSISTED_NBT_TAG).getBoolean("Vanished");
 
 			if (forPlayer != null) {
--- src/main/java/redstonedubstep/mods/vanishmod/mixin/world/MixinLivingEntity.java
+++ src/main/java/redstonedubstep/mods/vanishmod/mixin/world/MixinLivingEntity.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.server.level.ServerLevel;
@@ -27,7 +28,7 @@ public abstract class MixinLivingEntity extends Entity {
 	//Prevent particles from being created when a vanished player falls
 	@Redirect(method = "checkFallDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;sendParticles(Lnet/minecraft/core/particles/ParticleOptions;DDDIDDDD)I"))
 	public <T extends ParticleOptions> int redirectSendParticles(ServerLevel serverWorld, T type, double posX, double posY, double posZ, int particleCount, double xOffset, double yOffset, double zOffset, double speed) {
-		if (!VanishConfig.CONFIG.hidePlayersFromWorld.get() || !VanishUtil.isVanished(getCommandSenderWorld().getPlayerByUUID(getUUID())))
+		if (!VanishConfig.CONFIG.hidePlayersFromWorld.get() || !VanishUtil.isVanished(this))
 			serverWorld.sendParticles(type, posX, posY, posZ, particleCount, xOffset, yOffset, zOffset, speed);
 
 		return 0;
@@ -36,16 +37,23 @@ public abstract class MixinLivingEntity extends Entity {
 	//Prevent pickup animation from being sent when a vanished player picks up an item. This fixes that the unvanished client thinks that it picked up an item (and thus shows a pickup animation for the local player) while in reality a vanished player did
 	@Inject(method = "take", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerChunkCache;broadcast(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/network/protocol/Packet;)V"), cancellable = true)
 	public void redirectBroadcast(Entity entity, int amount, CallbackInfo callbackInfo) {
-		if (VanishConfig.CONFIG.hidePlayersFromWorld.get() && VanishUtil.isVanished(getCommandSenderWorld().getPlayerByUUID(getUUID())))
+		if (VanishConfig.CONFIG.hidePlayersFromWorld.get() && VanishUtil.isVanished(this))
 			callbackInfo.cancel();
 	}
 
 	//Suppress eating sound when a vanished player finishes eating
 	@ModifyArg(method = "eat", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;playSound(Lnet/minecraft/world/entity/player/Player;DDDLnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FF)V"))
 	private SoundEvent removeEatingSound(SoundEvent soundEvent) {
-		if (VanishConfig.CONFIG.hidePlayersFromWorld.get() && VanishUtil.isVanished(getCommandSenderWorld().getPlayerByUUID(getUUID())))
+		if (VanishConfig.CONFIG.hidePlayersFromWorld.get() && VanishUtil.isVanished(this))
 			soundEvent = null;
 
 		return soundEvent;
+	}
+
+	//Prevent entities like passive mobs or pufferfish from detecting vanished players
+	@Inject(method = "canBeSeenByAnyone", at = @At("HEAD"), cancellable = true)
+	public void onCanBeSeen(CallbackInfoReturnable<Boolean> callbackInfo) {
+		if (VanishConfig.CONFIG.hidePlayersFromWorld.get() && VanishUtil.isVanished(this))
+			callbackInfo.setReturnValue(false);
 	}
 }
