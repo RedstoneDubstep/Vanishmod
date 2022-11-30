061e795e709578bdde9633edfe967082f9f80684
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2022-11-30 21:59:01 +0100

    Removed unneccessary mixins

--- src/main/java/redstonedubstep/mods/vanishmod/mixin/world/MixinLivingEntity.java
+++ src/main/java/redstonedubstep/mods/vanishmod/mixin/world/MixinLivingEntity.java
@@ -3,13 +3,11 @@ package redstonedubstep.mods.vanishmod.mixin.world;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -31,15 +29,6 @@ public abstract class MixinLivingEntity extends Entity {
 			serverWorld.sendParticles(type, posX, posY, posZ, particleCount, xOffset, yOffset, zOffset, speed);
 
 		return 0;
-	}
-
-	//Suppress eating sound when a vanished player finishes eating
-	@ModifyArg(method = "eat", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;playSound(Lnet/minecraft/world/entity/player/Player;DDDLnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FF)V"))
-	private SoundEvent removeEatingSound(SoundEvent soundEvent) {
-		if (VanishConfig.CONFIG.hidePlayersFromWorld.get() && VanishUtil.isVanished(this))
-			soundEvent = null;
-
-		return soundEvent;
 	}
 
 	//Prevent entities like passive mobs or pufferfish from detecting vanished players
