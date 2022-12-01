b4f854c7c27c9de11091c09164317a8d5b93507a
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2022-12-01 22:21:41 +0100

    Fixed join/leave messages not being sent out to other vanished players

--- src/main/java/redstonedubstep/mods/vanishmod/mixin/MixinPlayerList.java
+++ src/main/java/redstonedubstep/mods/vanishmod/mixin/MixinPlayerList.java
@@ -1,17 +1,11 @@
 package redstonedubstep.mods.vanishmod.mixin;
 
-import java.util.UUID;
-
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.network.Connection;
-import net.minecraft.network.chat.ChatType;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundLevelEventPacket;
 import net.minecraft.network.protocol.game.ClientboundSoundEntityPacket;
@@ -21,42 +15,20 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.players.PlayerList;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.server.ServerLifecycleHooks;
 import redstonedubstep.mods.vanishmod.VanishConfig;
 import redstonedubstep.mods.vanishmod.VanishUtil;
+import redstonedubstep.mods.vanishmod.misc.FieldHolder;
 import redstonedubstep.mods.vanishmod.misc.SoundSuppressionHelper;
 
 @Mixin(value = PlayerList.class)
 public abstract class MixinPlayerList {
-	@Unique
-	private ServerPlayer joiningPlayer;
-
-	//Prevent join, leave, death and advancement messages of vanished players from being broadcast
-	@Inject(method = "broadcastMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/ChatType;Ljava/util/UUID;)V", at = @At(value = "HEAD"), cancellable = true)
-	public void redirectBroadcastMessage(Component text, ChatType chatType, UUID uuid, CallbackInfo callbackInfo) { //TODO this doesn't seem to cover death messages sent with team option "sendToAllExceptTeam" and similar, verify pls; also please for the love of god rename this
-		if (text instanceof TranslatableComponent component) {
-			if (component.getKey().startsWith("multiplayer.player.joined") && VanishUtil.isVanished(joiningPlayer)) {
-				joiningPlayer = null;
-				callbackInfo.cancel();
-			}
-			else if (component.getKey().startsWith("multiplayer.player.left") || component.getKey().startsWith("death.") || component.getKey().startsWith("chat.type.advancement")) {
-				if (component.getArgs()[0] instanceof Component playerName) {
-					for (ServerPlayer player : ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayers()) {
-						if (player.getDisplayName().getString().equals(playerName.getString()) && VanishUtil.isVanished(player))
-							callbackInfo.cancel();
-					}
-				}
-			}
-		}
-	}
-
-	//Vanishes any unvanished players that are on the vanishing queue. Also acts as a helper for accessing the player in question in the method above, as you cannot get it from PlayerList#broadcastMessage
+	//Vanishes any unvanished players that are on the vanishing queue. Also acts as a helper for accessing the player that is currently joining the server
 	@Inject(method = "placeNewPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;broadcastMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/ChatType;Ljava/util/UUID;)V"))
 	public void onSendJoinMessage(Connection networkManager, ServerPlayer player, CallbackInfo ci) {
 		if (VanishUtil.removeFromQueue(player.getGameProfile().getName()) && !VanishUtil.isVanished(player))
 			VanishUtil.toggleVanish(player);
 
-		joiningPlayer = player;
+		FieldHolder.joiningPlayer = player;
 	}
 
 	//Stores the player that is exempted from broadcasting a given sound packet, which most likely is the one causing the packet to be sent, so the information can be used later for sound suppression
--- src/main/java/redstonedubstep/mods/vanishmod/mixin/MixinServerGamePacketListenerImpl.java
+++ src/main/java/redstonedubstep/mods/vanishmod/mixin/MixinServerGamePacketListenerImpl.java
@@ -9,8 +9,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import io.netty.util.concurrent.GenericFutureListener;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundBlockEventPacket;
+import net.minecraft.network.protocol.game.ClientboundChatPacket;
 import net.minecraft.network.protocol.game.ClientboundLevelEventPacket;
 import net.minecraft.network.protocol.game.ClientboundPlayerInfoPacket;
 import net.minecraft.network.protocol.game.ClientboundPlayerInfoPacket.Action;
@@ -22,8 +26,10 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.server.ServerLifecycleHooks;
 import redstonedubstep.mods.vanishmod.VanishConfig;
 import redstonedubstep.mods.vanishmod.VanishUtil;
+import redstonedubstep.mods.vanishmod.misc.FieldHolder;
 import redstonedubstep.mods.vanishmod.misc.SoundSuppressionHelper;
 
 @Mixin(ServerGamePacketListenerImpl.class)
@@ -40,12 +46,14 @@ public class MixinServerGamePacketListenerImpl {
 	@Inject(method = "send(Lnet/minecraft/network/protocol/Packet;)V", at = @At("HEAD"), cancellable = true)
 	private void onSendPacket(Packet<?> packet, CallbackInfo callbackInfo) {
 		if (packet instanceof ClientboundPlayerInfoPacket infoPacket && infoPacket.getAction() != Action.REMOVE_PLAYER) {
-			List<PlayerUpdate> vanishedPacketEntries = infoPacket.getEntries().stream().filter(p -> VanishUtil.isVanished(server.getPlayerList().getPlayer(p.getProfile().getId()), player)).toList();
+			List<PlayerUpdate> filteredPacketEntries = infoPacket.getEntries().stream().filter(p -> !VanishUtil.isVanished(server.getPlayerList().getPlayer(p.getProfile().getId()), player)).toList();
 
-			if (vanishedPacketEntries.equals(infoPacket.getEntries()))
+			if (filteredPacketEntries.isEmpty())
 				callbackInfo.cancel();
-			else if (!vanishedPacketEntries.isEmpty())
-				infoPacket.getEntries().removeAll(vanishedPacketEntries);
+			else if (!filteredPacketEntries.equals(infoPacket.getEntries())) {
+				infoPacket.getEntries().clear();
+				infoPacket.getEntries().addAll(filteredPacketEntries);
+			}
 		}
 		else if (packet instanceof ClientboundTakeItemEntityPacket pickupPacket && VanishUtil.isVanished(player.level.getEntity(pickupPacket.getPlayerId()), player))
 			callbackInfo.cancel();
@@ -58,6 +66,25 @@ public class MixinServerGamePacketListenerImpl {
 				callbackInfo.cancel();
 			else if (packet instanceof ClientboundBlockEventPacket eventPacket && SoundSuppressionHelper.shouldSuppressSoundEventFor(null, player.level, Vec3.atCenterOf(eventPacket.getPos()), player))
 				callbackInfo.cancel();
+		}
+	}
+
+	//Prevent join, leave, death and advancement messages of vanished players from being broadcast
+	@Inject(method = "send(Lnet/minecraft/network/protocol/Packet;Lio/netty/util/concurrent/GenericFutureListener;)V", at = @At("HEAD"), cancellable = true)
+	private void onSendPacket(Packet<?> packet, GenericFutureListener<?> listener, CallbackInfo callbackInfo) {
+		if (packet instanceof ClientboundChatPacket chatPacket && chatPacket.getMessage() instanceof TranslatableComponent component) {
+			if (component.getKey().startsWith("multiplayer.player.joined") && VanishUtil.isVanished(FieldHolder.joiningPlayer)) {
+				FieldHolder.joiningPlayer = null;
+				callbackInfo.cancel();
+			}
+			else if (component.getKey().startsWith("multiplayer.player.left") || component.getKey().startsWith("death.") || component.getKey().startsWith("chat.type.advancement")) {
+				if (component.getArgs()[0] instanceof Component playerName) {
+					for (ServerPlayer sender : ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayers()) {
+						if (sender.getDisplayName().getString().equals(playerName.getString()) && VanishUtil.isVanished(sender, player))
+							callbackInfo.cancel();
+					}
+				}
+			}
 		}
 	}
 }
