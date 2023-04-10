adba2c7d38cd145bd4b7314366409cfdbe31b461
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2023-04-10 20:55:45 +0200

    Fixed potential crash when dying under specific circumstances

--- src/main/java/redstonedubstep/mods/vanishmod/mixin/chat/MixinCombatTracker.java
+++ src/main/java/redstonedubstep/mods/vanishmod/mixin/chat/MixinCombatTracker.java
@@ -4,6 +4,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.contents.TranslatableContents;
@@ -15,9 +16,9 @@ import redstonedubstep.mods.vanishmod.VanishUtil;
 @Mixin(CombatTracker.class)
 public abstract class MixinCombatTracker {
 	//Change the death message of an unvanished player to the generic one if it was killed by a vanished player
-	@ModifyVariable(method = "getDeathMessage", at = @At(value = "RETURN", shift = Shift.BEFORE), ordinal = 1)
+	@ModifyVariable(method = "getDeathMessage", slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/world/damagesource/CombatEntry;getAttackerName()Lnet/minecraft/network/chat/Component;")), at = @At(value = "RETURN", shift = Shift.BEFORE), ordinal = 1)
 	private Component modifyDeathMessage(Component deathMessage) {
-		if (deathMessage.getContents() instanceof TranslatableContents content && content.getArgs().length > 1 && content.getArgs()[1] instanceof Component playerName) {
+		if (deathMessage != null && deathMessage.getContents() instanceof TranslatableContents content && content.getArgs().length > 1 && content.getArgs()[1] instanceof Component playerName) {
 			for (ServerPlayer killer : ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayers()) {
 				if (killer.getDisplayName().getString().equals(playerName.getString()) && VanishUtil.isVanished(killer))
 					deathMessage = Component.translatable("death.attack.generic", content.getArgs()[0]);
