ade0274b9aa95f7e32517f5a678cf40bbd7942f0
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2023-06-26 21:22:44 +0200

    Fixed incorrect online player count of servers on multiplayer screen

--- Changelog.md
+++ Changelog.md
@@ -1,3 +1,7 @@
+-----------Changelog for v1.1.12-----------
+
+- Fixed incorrect online player count of servers with Vanishmod installed on the server list screen
+
 -----------Changelog for v1.1.11-----------
 
 - Updated to 1.20
--- src/main/java/redstonedubstep/mods/vanishmod/mixin/gui/ServerStatusPacketListenerImplMixin.java
+++ src/main/java/redstonedubstep/mods/vanishmod/mixin/gui/ServerStatusPacketListenerImplMixin.java
@@ -9,33 +9,50 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 import com.mojang.authlib.GameProfile;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.Util;
 import net.minecraft.network.protocol.status.ClientboundStatusResponsePacket;
 import net.minecraft.network.protocol.status.ServerStatus;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerStatusPacketListenerImpl;
 import net.minecraft.server.players.PlayerList;
+import net.minecraft.util.Mth;
 import net.minecraftforge.server.ServerLifecycleHooks;
 import redstonedubstep.mods.vanishmod.VanishConfig;
 import redstonedubstep.mods.vanishmod.VanishUtil;
 
 @Mixin(ServerStatusPacketListenerImpl.class)
 public class ServerStatusPacketListenerImplMixin {
-	//Stop server from sending the names of vanished players to the Multiplayer screen
+	//Updates the player list sent to clients on the Multiplayer screen to only display and count unvanished players
 	@Redirect(method = "handleStatusRequest", at = @At(value = "NEW", target = "(Lnet/minecraft/network/protocol/status/ServerStatus;Ljava/lang/String;)Lnet/minecraft/network/protocol/status/ClientboundStatusResponsePacket;"))
 	public ClientboundStatusResponsePacket vanishmod$constructSServerInfoPacket(ServerStatus status, String cachedStatus) {
 		if (VanishConfig.CONFIG.hidePlayersFromPlayerLists.get()) {
-			PlayerList list = ServerLifecycleHooks.getCurrentServer().getPlayerList();
-			Optional<ServerStatus.Players> players = status.players();
+			MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+			PlayerList list = server.getPlayerList();
+			List<ServerPlayer> unvanishedPlayers = VanishUtil.formatPlayerList(list.getPlayers(), null);
+			int unvanishedPlayerCount = unvanishedPlayers.size();
+			int maxPlayers = list.getMaxPlayers();
+			ServerStatus.Players playerStatus;
 
-			if (players.isPresent()) {
-				List<GameProfile> gameProfiles = players.get().sample();
+			if (server.hidesOnlinePlayers())
+				playerStatus = new ServerStatus.Players(maxPlayers, unvanishedPlayerCount, List.of());
+			else {
+				int playerSampleSize = unvanishedPlayers.size();
+				ObjectArrayList<GameProfile> newPlayers = new ObjectArrayList<>(Math.min(playerSampleSize, 12));
+				int offset = Mth.nextInt(server.overworld().random, 0, unvanishedPlayerCount - playerSampleSize);
 
-				List<GameProfile> newGameProfiles = gameProfiles.stream()
-						.filter(p -> !VanishUtil.isVanished(list.getPlayer(p.getId())))
-						.toList();
+				for(int l = 0; l < playerSampleSize; ++l) {
+					ServerPlayer serverplayer = unvanishedPlayers.get(offset + l);
+					newPlayers.add(serverplayer.allowsListing() ? serverplayer.getGameProfile() : MinecraftServer.ANONYMOUS_PLAYER_PROFILE);
+				}
 
-				status = new ServerStatus(status.description(), Optional.of(new ServerStatus.Players(players.get().max(), newGameProfiles.size() , newGameProfiles)), status.version(), status.favicon(), status.enforcesSecureChat(), status.forgeData());
-				cachedStatus = null;
+				Util.shuffle(newPlayers, server.overworld().random);
+				playerStatus = new ServerStatus.Players(maxPlayers, unvanishedPlayerCount, newPlayers);
 			}
+
+			status = new ServerStatus(status.description(), Optional.of(playerStatus), status.version(), status.favicon(), status.enforcesSecureChat(), status.forgeData());
+			cachedStatus = null;
 		}
 
 		return new ClientboundStatusResponsePacket(status, cachedStatus);
