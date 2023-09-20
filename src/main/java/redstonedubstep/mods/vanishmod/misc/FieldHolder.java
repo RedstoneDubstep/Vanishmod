40234d617983ab0258273b050ce585c0af3b3808
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2023-09-20 17:44:45 +0200

    v1.1.13: Fixed some mods sending messages mentioning vanished players
    also renamed a config field
    Fixes #19

--- src/main/java/redstonedubstep/mods/vanishmod/misc/FieldHolder.java
+++ src/main/java/redstonedubstep/mods/vanishmod/misc/FieldHolder.java
@@ -3,6 +3,8 @@ package redstonedubstep.mods.vanishmod.misc;
 import net.minecraft.server.level.ServerPlayer;
 
 public class FieldHolder {
-	//The player that is currently in the process of joining the server. Required due to new players not being added to PlayerList when the join message for them is sent
+	//The player that is currently in the process of joining the server. Required due to new players not being added to the regular player list when their join message is sent
 	public static ServerPlayer joiningPlayer;
+	//The player that is currently in the process of leaving the server. Required due to leaving players getting removed from the regular player list too early in some occasions
+	public static ServerPlayer leavingPlayer;
 }
