deb02ebf0ebec45ff4e20557aa0b2b5ff77a2bd9
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2023-10-16 20:37:58 +0200

    Optimised construction of server status that filters vanished players
    Fixes the bug reported in #21

--- src/main/java/redstonedubstep/mods/vanishmod/misc/FieldHolder.java
+++ src/main/java/redstonedubstep/mods/vanishmod/misc/FieldHolder.java
@@ -1,5 +1,6 @@
 package redstonedubstep.mods.vanishmod.misc;
 
+import net.minecraft.network.protocol.status.ServerStatus;
 import net.minecraft.server.level.ServerPlayer;
 
 public class FieldHolder {
@@ -7,4 +8,6 @@ public class FieldHolder {
 	public static ServerPlayer joiningPlayer;
 	//The player that is currently in the process of leaving the server. Required due to leaving players getting removed from the regular player list too early in some occasions
 	public static ServerPlayer leavingPlayer;
+	//The server status that is used when the config option hidePlayersFromPlayerLists is turned on and that accounts for vanished players
+	public static ServerStatus vanishedServerStatus;
 }
--- src/main/java/redstonedubstep/mods/vanishmod/mixin/gui/ServerStatusPacketListenerImplMixin.java
+++ src/main/java/redstonedubstep/mods/vanishmod/mixin/gui/ServerStatusPacketListenerImplMixin.java
@@ -1,57 +1,22 @@
 package redstonedubstep.mods.vanishmod.mixin.gui;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import com.mojang.authlib.GameProfile;
-
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import net.minecraft.Util;
 import net.minecraft.network.protocol.status.ClientboundStatusResponsePacket;
 import net.minecraft.network.protocol.status.ServerStatus;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerStatusPacketListenerImpl;
-import net.minecraft.server.players.PlayerList;
-import net.minecraft.util.Mth;
-import net.minecraftforge.server.ServerLifecycleHooks;
 import redstonedubstep.mods.vanishmod.VanishConfig;
-import redstonedubstep.mods.vanishmod.VanishUtil;
+import redstonedubstep.mods.vanishmod.misc.FieldHolder;
 
 @Mixin(ServerStatusPacketListenerImpl.class)
 public class ServerStatusPacketListenerImplMixin {
 	//Updates the player list sent to clients on the Multiplayer screen to only display and count unvanished players
 	@Redirect(method = "handleStatusRequest", at = @At(value = "NEW", target = "(Lnet/minecraft/network/protocol/status/ServerStatus;Ljava/lang/String;)Lnet/minecraft/network/protocol/status/ClientboundStatusResponsePacket;"))
 	public ClientboundStatusResponsePacket vanishmod$constructSServerInfoPacket(ServerStatus status, String cachedStatus) {
-		if (VanishConfig.CONFIG.hidePlayersFromPlayerLists.get()) {
-			MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
-			PlayerList list = server.getPlayerList();
-			List<ServerPlayer> unvanishedPlayers = VanishUtil.formatPlayerList(list.getPlayers(), null);
-			int unvanishedPlayerCount = unvanishedPlayers.size();
-			int maxPlayers = list.getMaxPlayers();
-			ServerStatus.Players playerStatus;
-
-			if (server.hidesOnlinePlayers())
-				playerStatus = new ServerStatus.Players(maxPlayers, unvanishedPlayerCount, List.of());
-			else {
-				int playerSampleSize = Math.min(unvanishedPlayers.size(), 12);
-				ObjectArrayList<GameProfile> newPlayers = new ObjectArrayList<>(playerSampleSize);
-				int offset = Mth.nextInt(server.overworld().random, 0, unvanishedPlayerCount - playerSampleSize);
-
-				for(int l = 0; l < playerSampleSize; ++l) {
-					ServerPlayer player = unvanishedPlayers.get(offset + l);
-					newPlayers.add(player.allowsListing() ? player.getGameProfile() : MinecraftServer.ANONYMOUS_PLAYER_PROFILE);
-				}
-
-				Util.shuffle(newPlayers, server.overworld().random);
-				playerStatus = new ServerStatus.Players(maxPlayers, unvanishedPlayerCount, newPlayers);
-			}
-
-			status = new ServerStatus(status.description(), Optional.of(playerStatus), status.version(), status.favicon(), status.enforcesSecureChat(), status.forgeData());
+		if (VanishConfig.CONFIG.hidePlayersFromPlayerLists.get() && FieldHolder.vanishedServerStatus != null) {
+			status = FieldHolder.vanishedServerStatus;
 			cachedStatus = null;
 		}
 
