ae32d748898e49cd98a6199013802c6d3f6323fc
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2025-07-13 12:03:08 +0200

    v1.1.18.1 - Fixed some mixins not applying

--- Changelog.md
+++ Changelog.md
@@ -1,3 +1,7 @@
+-----------Changelog for v1.1.18.1-----------
+
+- Fixed the "player detection mod compatibility" feature not working
+
 -----------Changelog for v1.1.18-----------
 
 - Ported to 1.21.5
--- src/main/java/redstonedubstep/mods/vanishmod/mixin/sound/EntityMixin.java
+++ src/main/java/redstonedubstep/mods/vanishmod/mixin/sound/EntityMixin.java
@@ -14,7 +14,7 @@ import redstonedubstep.mods.vanishmod.misc.SoundSuppressionHelper;
 @Mixin(Entity.class)
 public class EntityMixin {
 	//Invalidates the hit results of a vanished player if its position changes, because then their crosshair is most likely on a different block
-	@Inject(method = "move", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;setPos(DDD)V", ordinal = 1))
+	@Inject(method = "move", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;setPos(Lnet/minecraft/world/phys/Vec3;)V"))
 	private void vanishmod$onActualMove(MoverType type, Vec3 pos, CallbackInfo callbackInfo) {
 		if (SoundSuppressionHelper.shouldCapturePlayers() && (Object) this instanceof ServerPlayer player && !player.hasContainerOpen())
 			SoundSuppressionHelper.invalidateHitResults(player);
