97b1186db39155beb29cca81f8be828463a73353
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2025-08-16 17:52:12 +0200

    Fixed vanished players showing up on player locator bar

---
+++ src/main/java/redstonedubstep/mods/vanishmod/mixin/world/WaypointTransmitterMixin.java
@@ -0,0 +1,21 @@
+package redstonedubstep.mods.vanishmod.mixin.world;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.waypoints.WaypointTransmitter;
+import redstonedubstep.mods.vanishmod.VanishUtil;
+
+@Mixin(WaypointTransmitter.class)
+public interface WaypointTransmitterMixin {
+	//Prevents vanished players from showing up on the locator bar of players that aren't allowed to see them
+	@Inject(method = "doesSourceIgnoreReceiver", at = @At("HEAD"), cancellable = true)
+	private static void vanishmod$ignoreVanishedSources(LivingEntity source, ServerPlayer receiver, CallbackInfoReturnable<Boolean> callbackInfo) {
+		if (VanishUtil.isVanished(source, receiver))
+			callbackInfo.setReturnValue(true);
+	}
+}
