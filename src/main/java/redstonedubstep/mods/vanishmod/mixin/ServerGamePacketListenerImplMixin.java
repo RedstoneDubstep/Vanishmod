a15f08591fcfc3a305398e9f3e4dcebcc79d0c86
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2025-11-14 21:58:24 +0100

    Fixed some formatting issues related to tracing

--- src/main/java/redstonedubstep/mods/vanishmod/mixin/ServerGamePacketListenerImplMixin.java
+++ src/main/java/redstonedubstep/mods/vanishmod/mixin/ServerGamePacketListenerImplMixin.java
@@ -157,7 +157,7 @@ public class ServerGamePacketListenerImplMixin {
 					else if (VanishConfig.CONFIG.hidePlayerNameInSystemMessages.get()) {
 						Component replacement = Component.literal(VanishConfig.CONFIG.vanishedPlayerNameReplacement.get());
 
-						TraceHandler.trace(vanishedSender, "Player Name (now \"" + replacement.getString() + "\")", component.getString());
+						TraceHandler.trace(vanishedSender, Component.literal("Player Name (now \"").append(replacement).append("\")"), component.getString());
 						args[0] = replacement;
 					}
 
