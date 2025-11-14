c6d7e7e1f7bfd45da47a7527a8c15dba9e920ede
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2025-11-14 21:49:21 +0100

    v1.1.20 Hotfix - Fixed modded message filter not working sometimes

--- src/main/java/redstonedubstep/mods/vanishmod/misc/TraceHandler.java
+++ src/main/java/redstonedubstep/mods/vanishmod/misc/TraceHandler.java
@@ -59,14 +59,17 @@ public class TraceHandler {
 			visibleForComponent.append(Component.literal("§7(...)").withStyle(s -> s.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Currently: ").append(ComponentUtils.formatList(visibleForPlayerNames, ComponentUtils.DEFAULT_SEPARATOR))))));
 
 		player.sendSystemMessage(Component.literal("# §nTrace Status§r:"));
-		player.sendSystemMessage(Component.literal("# §bAlways enabled§r: ").append(Component.literal("§7(...)").withStyle(s -> s.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Hiding from the tab list and hiding your skin"))))));
+		player.sendSystemMessage(Component.literal("# §bAlways enabled§r: Hiding from the tab list and hiding your skin"));
 		player.sendSystemMessage(getTracePrefix(VanishConfig.CONFIG.hidePlayersFromWorld.get()).append("Hiding from the world, like through suppressing sounds and particles"));
 		player.sendSystemMessage(getTracePrefix(VanishConfig.CONFIG.hidePlayersFromPlayerLists.get()).append("Hiding from other player lists, like the server player list in the multiplayer screen"));
 		player.sendSystemMessage(getTracePrefix(VanishConfig.CONFIG.disableCommandTargeting.get()).append("Hiding from player selectors in commands, like the /give command"));
 		player.sendSystemMessage(getTracePrefix(VanishConfig.CONFIG.hideChatMessages.get()).append("Hiding chat and /teammsg messages"));
 		player.sendSystemMessage(getTracePrefix(VanishConfig.CONFIG.hideSystemMessages.get()).append("Hiding join, leave, advancement and death messages"));
 		player.sendSystemMessage(getTracePrefix(VanishConfig.CONFIG.hidePlayerNameInChat.get()).append("Hiding your player name in public chat messages, replacing it with \"").append(VanishConfig.CONFIG.vanishedPlayerNameReplacement.get()).append("\""));
-		player.sendSystemMessage(getTracePrefix(VanishConfig.CONFIG.hidePlayerNameInSystemMessages.get()).append("Hiding your player name in system messages, replacing it with \"").append(VanishConfig.CONFIG.vanishedPlayerNameReplacement.get()).append("\""));
+
+		if (!VanishConfig.CONFIG.hideSystemMessages.get())
+			player.sendSystemMessage(getTracePrefix(VanishConfig.CONFIG.hidePlayerNameInSystemMessages.get()).append("Hiding your player name in system messages, replacing it with \"").append(VanishConfig.CONFIG.vanishedPlayerNameReplacement.get()).append("\""));
+
 		player.sendSystemMessage(visibleForComponent);
 	}
 
